f7c4e970f7f0bb94ea8bc408d1306fecef61518b
Mauricio Valentim <mauriciovalentim.dev@gmail.com>
2025-07-01 22:45:46 -0300

    Learning about http requisitions

---
+++ 6_REQ_HTTP/src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+const url = "http://localhost:3000/products";
+import { useState, useEffect } from "react";
+function App() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch(url);
+
+            const data = await res.json();
+
+            setProducts(data);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <h1>Lista de Produtos</h1>
+            <ul>
+                {products.map(({ id, name, price }) => (
+                    <li key={id}>
+                        {name} - {price}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default App;
